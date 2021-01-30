7f5574c8a5611873dfea20e620ee15889596a798
Tiri <thierrylaval@gmx.com>
2021-01-30 17:09:54 +0100

    Travail du 30 janvier

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 /*
-	Paradigm Shift by HTML5 UP
-	html5up.net | @ajlkn
+	Thème modifié par Thierry Laval
+	Basé sur le théme de Paradigm Shift by HTML5 UP
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
